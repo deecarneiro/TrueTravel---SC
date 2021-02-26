decff5158e53f1975b211b29f0f7bc9d7f4b75a4
danita240697 <deysedayane.o.c@gmail.com>
2021-02-26 09:56:24 -0300

    New Structure

---
+++ src/main/java/model/managed/OrderMessageMB.java
@@ -0,0 +1,5 @@
+package model.managed;
+
+public class OrderMessageMB {
+
+}
--- src/main/java/model/persistence/VisitasPersistencia.java
+++ src/main/java/model/persistence/VisitasPersistencia.java
@@ -1,4 +1,4 @@
-package model.persistencia;
+package model.persistence;
 
 import java.util.HashSet;
 import java.util.Set;
